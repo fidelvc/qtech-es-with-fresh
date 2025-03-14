92d91f5098e7121aeb8a1c1943e2edde727fc3a6
Fidel VC <fidelvzcs@gmail.com>
2025-03-14 01:33:14 +0100

    wip add provisional semantic html structure

--- routes/contactar.tsx
+++ routes/contactar.tsx
@@ -2,9 +2,22 @@ import Form from "../islands/Form.tsx";
 
 export default function FormPage() {
   return (
-    <div>
-      <h1>Contact Form</h1>
-      <Form />
+    <div class="page">
+      <section class="contant-info">
+        <article>
+          <header>
+            <h1>Medios de contacto</h1>
+          </header>
+        </article>
+      </section>
+      <section class="contact-form">
+        <article>
+          <header>
+            <h2>Formulario de contacto</h2>
+            <Form></Form>
+          </header>
+        </article>
+      </section>
     </div>
   );
 }
