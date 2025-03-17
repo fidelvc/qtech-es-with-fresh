9c9793ff5745ef93cd15c06fc0e2eb2c8fbea89a
Fidel VC <fidelvzcs@gmail.com>
2025-03-17 01:19:16 +0100

    wip global css and page content and structure

--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -3,6 +3,7 @@ import { PageProps } from "$fresh/server.ts";
 export default function Layout({ Component }: PageProps) {
   return (
     <div class="layout">
+      
       <nav class="navbar">
         <a href="/">Inicio</a>
         <a href="/servicios">Servicios</a>
--- routes/index.tsx
+++ routes/index.tsx
@@ -8,6 +8,82 @@ export default function Home() {
           de aparatología estética multimarca, estamos en barcelona y damos
           soporte a nivel nacional
         </p>
+        <p>
+          Somos un servicio técnico dedicado al mantenimiento y puesta a punto
+          de aparatología estética multimarca, estamos en barcelona y damos
+          soporte a nivel nacional
+        </p>
+        <p>
+          Somos un servicio técnico dedicado al mantenimiento y puesta a punto
+          de aparatología estética multimarca, estamos en barcelona y damos
+          soporte a nivel nacional
+        </p>
+        <p>
+          Somos un servicio técnico dedicado al mantenimiento y puesta a punto
+          de aparatología estética multimarca, estamos en barcelona y damos
+          soporte a nivel nacional
+        </p>
+        <p>
+          Somos un servicio técnico dedicado al mantenimiento y puesta a punto
+          de aparatología estética multimarca, estamos en barcelona y damos
+          soporte a nivel nacional
+        </p>
+      </header>
+      <header>
+        <h1>Qtech</h1>
+        <p>
+          Somos un servicio técnico dedicado al mantenimiento y puesta a punto
+          de aparatología estética multimarca, estamos en barcelona y damos
+          soporte a nivel nacional
+        </p>
+        <p>
+          Somos un servicio técnico dedicado al mantenimiento y puesta a punto
+          de aparatología estética multimarca, estamos en barcelona y damos
+          soporte a nivel nacional
+        </p>
+        <p>
+          Somos un servicio técnico dedicado al mantenimiento y puesta a punto
+          de aparatología estética multimarca, estamos en barcelona y damos
+          soporte a nivel nacional
+        </p>
+        <p>
+          Somos un servicio técnico dedicado al mantenimiento y puesta a punto
+          de aparatología estética multimarca, estamos en barcelona y damos
+          soporte a nivel nacional
+        </p>
+        <p>
+          Somos un servicio técnico dedicado al mantenimiento y puesta a punto
+          de aparatología estética multimarca, estamos en barcelona y damos
+          soporte a nivel nacional
+        </p>
+      </header>
+      <header>
+        <h1>Qtech</h1>
+        <p>
+          Somos un servicio técnico dedicado al mantenimiento y puesta a punto
+          de aparatología estética multimarca, estamos en barcelona y damos
+          soporte a nivel nacional
+        </p>
+        <p>
+          Somos un servicio técnico dedicado al mantenimiento y puesta a punto
+          de aparatología estética multimarca, estamos en barcelona y damos
+          soporte a nivel nacional
+        </p>
+        <p>
+          Somos un servicio técnico dedicado al mantenimiento y puesta a punto
+          de aparatología estética multimarca, estamos en barcelona y damos
+          soporte a nivel nacional
+        </p>
+        <p>
+          Somos un servicio técnico dedicado al mantenimiento y puesta a punto
+          de aparatología estética multimarca, estamos en barcelona y damos
+          soporte a nivel nacional
+        </p>
+        <p>
+          Somos un servicio técnico dedicado al mantenimiento y puesta a punto
+          de aparatología estética multimarca, estamos en barcelona y damos
+          soporte a nivel nacional
+        </p>
       </header>
     </div>
   );
